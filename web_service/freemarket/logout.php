179e73a63630e2aa1c9535cdefcaa078f7264bf7
Y-UCHIDA\owner <kuronokurujp@gmail.com>
2019-01-03 19:02:21 +0900

    [modify]ログアウト機能を追加

--- web_service/freemarket/logout.php
+++ web_service/freemarket/logout.php
@@ -1,3 +1,19 @@
 <?php
 
+    // 共通関数を呼ぶ
+    require('function.php');
+
+    // ログ開始！
+    debug('---------------------------------');
+    debug('ログアウトページ');
+    debug('---------------------------------');
+    debugLogStart();
+
+    // セッションを削除
+    debug('ログアウトをします。');
+    session_destroy();
+
+    // ログインページに
+    debug('ログインページに遷移します。');
+    header('Location:login.php');
 ?>
