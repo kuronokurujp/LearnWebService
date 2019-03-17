97d3b0ffccb2a212a66c701ba4de9d4fce990548
Y-UCHIDA\owner <kuronokurujp@gmail.com>
2019-03-18 01:51:33 +0900

    [modify]cssファイルの配置変更

--- web_service/freemarket/head.php
+++ web_service/freemarket/head.php
@@ -5,6 +5,6 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title><?php echo $siteTitle; ?></title>
-        <link rel="stylesheet" type="text/css" href="mock/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
         <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     </head>
