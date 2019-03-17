7a1e5d2ef509313001cc55a8bc36a506b6699797
Y-UCHIDA\owner <kuronokurujp@gmail.com>
2019-03-18 01:50:54 +0900

    [modify]販売履歴のページファイルの配置を変更

---
+++ web_service/freemarket/tranSale.php
@@ -0,0 +1,32 @@
+<?php
+    $siteTitle = '販売履歴';
+    require('head.php');
+?>
+    <style>
+      .form .btn{
+        float: none;
+      }
+      .form{
+        text-align: center;
+      }
+    </style>
+
+  <body class="page-withdraw page-1colum">
+
+    <!-- メニュー -->
+<?php
+    require('header.php');
+?>
+    <!-- メインコンテンツ -->
+    <div id="contents" class="site-width">
+      <!-- Main -->
+      <section id="main" >
+        <h1 class="page-title">販売履歴</h1>
+        <a href="mypage.php">&lt; マイページに戻る</a>
+      </section>
+    </div>
+
+    <!-- footer -->
+<?php
+    require('footer.php');
+?>
