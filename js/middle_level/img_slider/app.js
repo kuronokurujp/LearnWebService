3e8fdea8abc854178302e14e80d69a283fb74876
Y-UCHIDA\owner <kuronokurujp@gmail.com>
2019-06-22 19:12:50 +0900

    [modify]画像スライド処理をクロージャーの書き方に変更

--- js/middle_level/img_slider/app.js
+++ js/middle_level/img_slider/app.js
@@ -1,24 +1,48 @@
-var currentItemNum = 1;
-var $slideContainer = $('.slider__container');
-var slideItemNum = $('.slider__item').length;
-var slideItemWidth = $('.slider__item').innerWidth();
-var slideContainerWidth = slideItemWidth * slideItemNum;
-// 0.5s期間アニメする
-var DURATION = 500;
+// クロージャーを使用した書き方
+var slider = (function() {
 
-$slideContainer.attr('style', 'width:' + slideContainerWidth + 'px');
+    var currentItemNum = 1;
+    var $slideContainer = $('.slider__container');
+    var slideItemNum = $('.slider__item').length;
+    var slideItemWidth = $('.slider__item').innerWidth();
+    var slideContainerWidth = slideItemWidth * slideItemNum;
+    // 0.5s期間アニメする
+    var DURATION = 500;
 
-$('.js-slide-prev').on('click', function() {
-    if (1 < currentItemNum) {
-        $slideContainer.animate({left: '+=' + slideItemWidth + 'px'}, DURATION);
-        --currentItemNum;
-    }
-});
+    return {
 
-$('.js-slide-next').on('click', function() {
-    if (currentItemNum < slideItemNum) {
-        $slideContainer.animate({left: '-=' + slideItemWidth + 'px'}, DURATION);
-        ++currentItemNum;
-    }
-});
+        // 左にスライドする
+        prevSlide: function() {
+            if (1 < currentItemNum) {
+                // 画像の要素横幅分ずらす
+                $slideContainer.animate({left: '+=' + slideItemWidth + 'px'}, DURATION);
+                --currentItemNum;
+            }
+        },
 
+        // 右にスライドする
+        nextSlide: function() {
+            if (currentItemNum < slideItemNum) {
+                // 画像の要素横幅分ずらす
+                $slideContainer.animate({left: '-=' + slideItemWidth + 'px'}, DURATION);
+                ++currentItemNum;
+            }
+        },
+
+        init: function() {
+            // 要素を横並びにする
+            $slideContainer.attr('style', 'width:' + slideContainerWidth + 'px');
+            var that = this;
+
+            $('.js-slide-prev').on('click', function() {
+                that.prevSlide();
+            });
+
+            $('.js-slide-next').on('click', function() {
+                that.nextSlide();
+            });
+        }
+    };
+})();
+
+slider.init();
